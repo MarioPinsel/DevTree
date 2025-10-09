67115a64b57364bff5f6a054c1a492ff5fc6f8de
Alejandro Pinto <pmlaser25@gmail.com>
2025-10-09 18:21:20 -0500

    Front

--- Frontend/src/views/LoginView.tsx
+++ Frontend/src/views/LoginView.tsx
@@ -1,5 +1,18 @@
+import { Link } from "react-router-dom"
+
 export default function LoginView() {
+
     return (
-        <div>LoginView</div>
+        <>
+            <div className=' bg-slatet-800 min-h-screen'>
+                <div className=' max-w-lg mx-auto pt-10 px-5'></div>
+                <img src='/logo.svg' alt='Logotipo' />
+            </div>
+            <nav>
+                <Link to="/auth/register">
+                    ¿No tienes cuenta? Crea una aqui
+                </Link>
+            </nav>
+        </>
     )
 }
--- Frontend/src/views/RegisterView.tsx
+++ Frontend/src/views/RegisterView.tsx
@@ -1,5 +1,14 @@
+import { Link } from "react-router-dom"
 export default function RegisterView() {
+
   return (
-    <div>RegisterView</div>
+    <>
+      <div>LoginView</div>
+      <nav>
+        <Link to="/auth/login">
+          ¿Ya tienes una cuenta? Inicia sesion
+        </Link>
+      </nav>
+    </>
   )
 }
