d58ca5e0a80a352d7f815f89591296340cf92528
Mariopinsel <pmlaser25@gmail.com>
2026-09-11 23:50:03 -0500

    Update 2v

--- Frontend/src/api/DevTreeAPI.ts
+++ Frontend/src/api/DevTreeAPI.ts
@@ -1,11 +1,14 @@
 import { isAxiosError } from 'axios'
 import api from '../config/axios'
+import type { User } from '../types/User'
 export async function getUser() {
+
     try {
-        const { data } = await api('/user')
+        const { data } = await api<User>('/user')
+      return data
     } catch (error) {
         if (isAxiosError(error) && error.response) {
-            console.log(error)
+            throw new Error(error.response.data.error)
         }
     }
 }
